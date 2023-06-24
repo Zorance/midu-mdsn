df49e3a9d71484c54470329f815eb6ad247b666e
Zorance <zorance@yeah.net>
2023-06-24 12:01:24 +0800

    feat: 创建dev分支.

---
+++ src/main/java/com/zorance/Dev.java
@@ -0,0 +1,20 @@
+/**
+ * FileName Dev.java
+ * Author Zorance
+ * Date 2023/6/24 12:00
+ */
+
+package com.zorance;
+
+/**
+ * @author DengYuZhu
+ * @version 2.0
+ * @description
+ * @date 2023/6/24 12:00
+ */
+public class Dev {
+
+    public static void main(String[] args) {
+        System.out.println("dev分支创建Dev类.");
+    }
+}
