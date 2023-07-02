6e9efcbf39824d08222ae9e3762106f2211533cb
Zorance <zorance@yeah.net>
2023-07-02 23:03:39 +0800

    feat: TestRequirement2開發

---
+++ src/main/java/com/zorance/TestRequirement2.java
@@ -0,0 +1,16 @@
+/**
+ * FileName TestRequirement2.java
+ * Author Zorance
+ * Date 2023/7/2 23:03
+ */
+
+package com.zorance;
+
+/**
+ * @author DengYuZhu
+ * @version 2.0
+ * @description
+ * @date 2023/7/2 23:03
+ */
+public class TestRequirement2 {
+}
