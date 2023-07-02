72495528a9213eb99a49f768c3dfecf925136c66
Zorance <zorance@yeah.net>
2023-07-02 23:06:20 +0800

    feat: TestRequirement1需求修改

--- src/main/java/com/zorance/TestRequirement1.java
+++ src/main/java/com/zorance/TestRequirement1.java
@@ -13,4 +13,9 @@ package com.zorance;
  * @date 2023/7/2 23:01
  */
 public class TestRequirement1 {
+
+    public static void main(String[] args) {
+        System.out.println("TestRequirement1 test分支開發.");
+    }
+
 }
