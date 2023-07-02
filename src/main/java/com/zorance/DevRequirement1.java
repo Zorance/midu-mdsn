fba37c134a259a43a46375c1b8b4ec83414d32c3
Zorance <zorance@yeah.net>
2023-07-02 23:11:33 +0800

    feat: DevRequirement1需求开发

---
+++ src/main/java/com/zorance/DevRequirement1.java
@@ -0,0 +1,16 @@
+/**
+ * FileName DevRequirement1.java
+ * Author Zorance
+ * Date 2023/7/2 23:11
+ */
+
+package com.zorance;
+
+/**
+ * @author DengYuZhu
+ * @version 2.0
+ * @description
+ * @date 2023/7/2 23:11
+ */
+public class DevRequirement1 {
+}
